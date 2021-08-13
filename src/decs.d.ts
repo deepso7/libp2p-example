1e2de04039ff5d4d26f4b289bd3343c7632cdb88
deepso <deeps.o7o8.y@gmail.com>
2021-08-13 21:33:43 +0530

    add peer discovery

--- src/decs.d.ts
+++ src/decs.d.ts
@@ -1,1 +1,2 @@
 declare module "libp2p-mplex";
+declare module "libp2p-websockets";
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import Libp2p from "libp2p";
-import TCP from "libp2p-tcp";
+// import TCP from "libp2p-tcp";
+import WebSockets from "libp2p-websockets";
 import { NOISE } from "libp2p-noise";
 import MPLEX from "libp2p-mplex";
+// import { multiaddr } from "multiaddr";
+import Bootstrap from "libp2p-bootstrap";
 
-import { multiaddr } from "multiaddr";
+const bootstrapMultiaddrs = [
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+];
 
 (async () => {
   try {
@@ -12,29 +18,33 @@ import { multiaddr } from "multiaddr";
         listen: ["/ip4/0.0.0.0/tcp/0"],
       },
       modules: {
-        transport: [TCP],
+        transport: [WebSockets],
         connEncryption: [NOISE],
         streamMuxer: [MPLEX],
+        peerDiscovery: [Bootstrap],
+      },
+      config: {
+        peerDiscovery: {
+          autoDial: true, // Auto connect to discovered peers (limited by ConnectionManager minConnections)
+          // The `tag` property will be searched when creating the instance of your Peer Discovery service.
+          // The associated object, will be passed to the service when it is instantiated.
+          [Bootstrap.tag]: {
+            enabled: true,
+            list: bootstrapMultiaddrs, // provide array of multiaddrs
+          },
+        },
       },
     });
 
     await node.start();
 
-    console.log("listening on addresses:");
-    console.log({ mutiaddrs: node.multiaddrs });
-
-    node.multiaddrs.forEach((addr) => {
-      console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`);
+    node.on("peer:discovery", (peer) => {
+      console.log("Discovered %s", peer.id.toB58String()); // Log discovered peer
     });
 
-    if (process.argv.length >= 3) {
-      const ma = multiaddr(process.argv[2]);
-      console.log(`pinging remote peer at ${process.argv[2]}`);
-      const latency = await node.ping(ma);
-      console.log(`pinged ${process.argv[2]} in ${latency}ms`);
-    } else {
-      console.log("no remote peer address given, skipping ping");
-    }
+    node.connectionManager.on("peer:connect", (connection) => {
+      console.log("Connected to %s", connection.remotePeer.toB58String()); // Log connected peer
+    });
 
     const stop = async () => {
       // stop libp2p
